8d5ab1946aec6830eb60fbdc24176f9ab010612d
shmibblez <shmibblez@gmail.com>
2025-05-08 20:21:22 -0400

    organizing settings

--- app/src/main/java/com/shmibblez/inferno/settings/compose/SettingsPage.kt
+++ app/src/main/java/com/shmibblez/inferno/settings/compose/SettingsPage.kt
@@ -9,6 +9,25 @@ private const val SCROLL_INDICATOR_DELAY = 10L
 private const val FXA_SYNC_OVERRIDE_EXIT_DELAY = 2000L
 private const val AMO_COLLECTION_OVERRIDE_EXIT_DELAY = 3000L
 
+// todo: settings page hierarchy:
+//  - toolbar item
+//    - address bar settings item
+//      - address bar settings page
+//    - toolbar icons list
+//  - tabs item
+//    - general title
+//      - close tabs automatically after item
+//        - never
+//        - one day
+//        - one week
+//        - one month
+//        - custom [whole number input] [day/week/month selector] max is 12 months
+//    - tab tray title
+//      - grid or list selector
+//    - tab bar title
+//      - show x to close
+//        - all tabs
+//        - active tab
 @Composable
 fun SettingsPage() {
     val context = LocalContext.current
